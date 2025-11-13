81cc0de712a34544b515d62184ec96dfa928e12d
Michael Souza <mssantos.k2@gmail.com>
2025-11-12 23:54:35 -0300

    Add download functionality and update settings for product retrieval

--- app/services/copernicus_service.py
+++ app/services/copernicus_service.py
@@ -1,8 +1,14 @@
 import os
+import sys
 import json
 import requests
+from datetime import date, timedelta
 from shapely.geometry import shape
 from dotenv import load_dotenv, find_dotenv
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
 
 from app.config.settings import SETTINGS
 
@@ -20,6 +26,7 @@ https://api.spectator.earth/?language=Python#spectator-api-docs
 https://app.spectator.earth/?&@-21.207458730482642,-35.50781250000001,3z
 
 """
+
 
 def get_access_token():
     token_url = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
@@ -63,22 +70,33 @@ def get_bounding_boxes(start_date: str = "2025-11-05T00:00:00.000Z", end_date: s
     return data["value"]
 
 
-# Download do produto
-# --------------------------------
-# url = f"https://download.dataspace.copernicus.eu/odata/v1/Products({json['value'][0]['Id']})/$value"
+def download_product(product_id: str, product_name: str, access_token: str, download_path: str):    
+    download_url = f"https://download.dataspace.copernicus.eu/odata/v1/Products({product_id})/$value"
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+    response = requests.get(download_url, headers=headers, stream=True)
+    response.raise_for_status()
 
-# headers = {"Authorization": f"Bearer {access_token}"}
+    filename = os.path.join(download_path, f"{product_name}.zip")
+    with open(filename, "wb") as f:
+        for chunk in response.iter_content(chunk_size=8192):
+            f.write(chunk)
+    return filename
 
-# session = requests.Session()
-# session.headers.update(headers)
 
-# response = session.get(url, stream=True)
+def start_download_products():
+    access_token = get_access_token()
+    yesterday = (date.today() - timedelta(1)).isoformat()
+    products = get_bounding_boxes(start_date=f"{yesterday}T00:00:00.000Z", end_date=f"{yesterday}T23:59:59.000Z")
+    download_path = SETTINGS["download"]["path"]
 
-# if response.status_code == 200:
-#     with open(f"{json['value'][0]['Name']}.zip", "wb") as file:
-#         for chunk in response.iter_content(chunk_size=8192):
-#             if chunk: 
-#                 file.write(chunk)
-# else:
-#     print(f"Erro ao fazer download. HTTP Status: {response.status_code}")
-#     print(response.text)
+    for product in products:
+        product_id = product["Id"]
+        product_name = product["Name"]
+        filename = download_product(product_id, product_name, access_token, download_path)
+        print(f"Produto {product_name} baixado em {filename}")
+
+
+if __name__ == "__main__":
+    start_download_products()
